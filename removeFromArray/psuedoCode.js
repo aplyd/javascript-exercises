9bf9d1fe542ff5259952337e8ffdcd6b599f8076
aplyd <austin@ftacnik.com>
2019-07-30 17:14:47 -0700

    use foreach and includes

--- removeFromArray/psuedoCode.js
+++ removeFromArray/psuedoCode.js
@@ -1,17 +1,19 @@
-const removeFromArray = function(arr, x) {
-    let reducedArr = [];
-    
-        for (i = 0; i < Array.from(arguments).length; i++) {
-            console.log('hi')
-            /*for (j = 0; j < arr.length; j++) {
-                if (arr[j] !== arguments[j]) {
-                    reducedArr.push(arr[j]);
-                }
-            }*/
+const removeFromArray = function(...args) {
+    let array = args[0];
+    let reducedArray = [];
+
+    array.forEach((element) => {
+
+        if (!args.includes(element)) {
+            reducedArray.push(element);
         }
-    return reducedArr;
+
+    });
+
+    return reducedArray;
 }
 
 console.log (
-removeFromArray([1,2,3,4], 2, 1, 5, 7, 8)
+removeFromArray([1,2,3,4], 2, 1)
+
 )
--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args[0];
+    let reducedArray = [];
 
+    array.forEach((element) => {
+
+        if (!args.includes(element)) {
+            reducedArray.push(element);
+        }
+
+    });
+
+    return reducedArray;
 }
 
 module.exports = removeFromArray
